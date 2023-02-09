be3f2dee5939fe6701b1bdf09d36bd32027abb94
Daniil Davydovskiy <danildad.dad@yandex.ru>
2023-02-09 19:21:20 +0300

    feat: добавить разметку для логина

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,22 @@
 export default function Login() {
-    return (
-      <div className="auth"></div>
-    );
-  }
+  return (
+    <div className="auth">
+      <h2 className="auth__title">Вход</h2>
+      <form className="auth__form">
+        <input
+          className="auth__input auth__input_type_email"
+          autoComplete="off"
+          placeholder="Email"
+          type={'email'}
+        />
+        <input
+          className="auth__input auth__input_type_password"
+          autoComplete="off"
+          placeholder="Пароль"
+          type={'password'}
+        />
+        <button className="auth__button">Войти</button>
+      </form>
+    </div>
+  );
+}
