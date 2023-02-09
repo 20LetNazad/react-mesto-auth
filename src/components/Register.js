af25824f917474f32dd42e7379bf981f2d3ceccd
Daniil Davydovskiy <danildad.dad@yandex.ru>
2023-02-09 19:09:16 +0300

    feat: добавить разметку для регистрации

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,30 @@
+import { Link } from 'react-router-dom';
+
 export default function Register() {
-    return (
-      <div className="auth">
-        
-      </div>
-    );
-  }
+  return (
+    <div className="auth">
+      <h2 className="auth__title">Регистрация</h2>
+      <form className="auth__form">
+        <input
+          className="auth__input auth__input_type_email"
+          autoComplete="off"
+          placeholder="Email"
+          type={'email'}
+        />
+        <input
+          className="auth__input auth__input_type_password"
+          autoComplete="off"
+          placeholder="Пароль"
+          type={'password'}
+        />
+        <button className="auth__button">Зарегистрироваться</button>
+      </form>
+      <p className="auth__subtitle">
+        Уже зарегистрированы?
+        <Link to={'/sign-in'} className="auth__link">
+          Войти
+        </Link>
+      </p>
+    </div>
+  );
+}
